388c75237f2bef7e6067c3d29bb53ddedec588b2
Antonio Marchese <01645194400@estudante.ufjf.br>
2023-01-16 12:39:02 -0300

    adicionando rota para registro de usuário

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { userResourceOptions } from "./adminjs/resources/user";
+import { authController } from "./controllers/auth.controller";
 import { categoriesController } from "./controllers/categories.controller";
 import { productsController } from "./controllers/products.controller";
 
@@ -9,5 +11,7 @@ router.get("/categories/:id", categoriesController.show);
 
 router.get("/products", productsController.index);
 router.get("/products/:id", productsController.show);
+
+router.post("/auth/register", authController.register);
 
 export { router };
